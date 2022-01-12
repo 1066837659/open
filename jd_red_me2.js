2e9948d4ca5283449fb0923df436ee44b98c06be
gugu1066 <83374355+1066837659@users.noreply.github.com>
2022-01-13 07:46:15 +0800

    Update jd_red_me2.js

--- jd_red_me2.js
+++ jd_red_me2.js
@@ -22,7 +22,7 @@ cron "0 0,12,20,21 * * *" script-path=https://raw.githubusercontent.com/KingRan/
 
 const $ = new Env('京东领红包');
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
-let flCode = ["SI6n9s9", "SK60cUr", "SM66Msb"]
+let flCode = ["SLyyO69", "SIyX1Lg"]
 flCode = flCode[Math.floor((Math.random() * flCode.length))]
 if ($.isNode() && process.env.FLCODE3) {
     flCode = process.env.FLCODE3
