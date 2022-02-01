077c763461931d68a4ca9ad18678756caa0701ad
gugu1066 <83374355+1066837659@users.noreply.github.com>
2022-02-01 10:11:29 +0800

    Update jd_disable.py

--- jd_disable.py
+++ jd_disable.py
@@ -28,7 +28,7 @@ if not ipport:
     ipport = "localhost:5700"
 else:
     ipport = ipport.lstrip("http://").rstrip("/")
-sub_str = os.getenv("RES_SUB", "KingRan_JDJB")
+sub_str = os.getenv("1066837659_open", "yuannian1112")
 sub_list = sub_str.split("&")
 res_only = os.getenv("RES_ONLY", True)
 headers = {
